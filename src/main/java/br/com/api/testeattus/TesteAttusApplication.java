e263b77b9c88a26a40cb6c2951784ff9662b61dd
gemeoslemes <victorlemes0776@gmail.com>
2024-04-24 13:49:50 -0300

    Projeto inicial

---
+++ src/main/java/br/com/api/testeattus/TesteAttusApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.testeattus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAttusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAttusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/testeattus/TesteAttusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.testeattus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAttusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
